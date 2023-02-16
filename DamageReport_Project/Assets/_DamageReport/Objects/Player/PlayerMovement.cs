fb97ca1b1a252d006d17d26bb84329e9edf47807
Alexander Zaikin <ay.zaikin@gmail.com>
2023-02-16 10:12:03 +0500

    CAR_GAME-6 Сделать направление движения зависящим от угла поворота камеры

--- DamageReport_Project/Assets/_DamageReport/Objects/Player/PlayerMovement.cs
+++ DamageReport_Project/Assets/_DamageReport/Objects/Player/PlayerMovement.cs
@@ -4,10 +4,19 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private ReadVariable<Vector2> inputDirection;
     [SerializeField] private float speed;
+    [SerializeField] private new Transform camera;
 
     private void FixedUpdate()
     {
-        Vector3 moveDirection = new(inputDirection.Get().x, 0, inputDirection.Get().y);
+        if (inputDirection == Vector2.zero)
+        {
+            return;
+        }
+        var worldUp = transform.position - camera.position;
+        worldUp.y = 0;
+        worldUp.Normalize();
+        var moveAngle = Mathf.Atan2(inputDirection.Get().y, inputDirection.Get().x) * Mathf.Rad2Deg;
+        var moveDirection = Quaternion.Euler(0, -moveAngle + 90, 0) * worldUp;
         transform.position += speed * Time.fixedDeltaTime * moveDirection;
     }
 }

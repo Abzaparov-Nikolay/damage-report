9c42fd3f623202ecd9c0bccc31a04ede385c9960
Alexander Zaikin <ay.zaikin@gmail.com>
2023-05-02 14:59:10 +0500

    Исправлены баги: следы шин мигали при получении урона; эффект получения урона был прозрачным

--- DamageReport_Project/Assets/_DamageReport/Objects/Entities/Blinking.cs
+++ DamageReport_Project/Assets/_DamageReport/Objects/Entities/Blinking.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class Blinking : MonoBehaviour
@@ -12,7 +13,7 @@ public class Blinking : MonoBehaviour
     [ContextMenu("Start")]
     public void StartBlinking()
     {
-        var renderers = gameObject.GetComponentsInChildren<Renderer>();
+        var renderers = gameObject.GetComponentsInChildren<MeshRenderer>();
         if(isBlinking)
             return;
         else

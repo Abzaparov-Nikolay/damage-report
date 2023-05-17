e677857cc966d8a8bb5f754b58d8d2965f4ba206
Abzaparov-Nikolay <nd.abzaparov@gmail.com>
2023-05-17 19:51:03 +0500

    CAR_GAME-28 Добавить эффект прозрачности для объектов, за которые заезжает машина Часть 1

---
+++ DamageReport_Project/Assets/_DamageReport/Objects/LookThroughWalls.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookThroughWalls : MonoBehaviour
+{
+	public Reference<Transform> player;
+	public RaycastHit[] hits = new RaycastHit[8];
+	public LayerMask layer;
+	public float raycastDistance;
+
+	private void Update()
+	{
+		var hittedAmount = Physics.RaycastNonAlloc(gameObject.transform.position, player.Value.position.normalized, hits, raycastDistance, layer);
+		for (var i = 0; i < hittedAmount; i++)
+		{
+			if(hits[i].collider.gameObject.TryGetComponent<TransparencyChanger>(out var changer))
+			{
+
+			}
+		}
+	}
+}

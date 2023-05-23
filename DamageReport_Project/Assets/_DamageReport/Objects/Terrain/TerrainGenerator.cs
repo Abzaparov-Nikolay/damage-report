e086ef74cc35954650ecc57809a86935d8458d71
Abzaparov-Nikolay <nd.abzaparov@gmail.com>
2023-05-23 22:13:16 +0500

    CAR_GAME-55 Сделать бесконечные пиллары и заменить обычные пиллары на префабы

--- DamageReport_Project/Assets/_DamageReport/Objects/Terrain/TerrainGenerator.cs
+++ DamageReport_Project/Assets/_DamageReport/Objects/Terrain/TerrainGenerator.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class TerrainGenerator : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	[SerializeField] private float Width = 300;
+	[SerializeField] private float xPos = 0;
+	[SerializeField] private float Length = 500;
+	[SerializeField] private float zPos = 0;
+	[SerializeField] private int pillarsSpawnAmount = 100;
+	[SerializeField] private int raycastTries = 2;
+	[SerializeField] private float raycastSphereRadius = 10;
+	public LayerMask layer;
+	public GameObject pillarPrefab;
+	public Transform TerrainParent;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	private RaycastHit[] hits = new RaycastHit[1];
+	void Start()
+	{
+		for (var j = 0; j < pillarsSpawnAmount; j++)
+		{
+			for (var i = 0; i < raycastTries; i++)
+			{
+				var position = new Vector3(xPos + (Width * (Random.value - 0.5f)), 0, zPos + (Length * (Random.value - 0.5f)));
+				var hitsCount = Physics.SphereCastNonAlloc(position, raycastSphereRadius, Vector3.one.normalized, hits, 0, layer);
+				if (hitsCount == 0)
+				{
+					Instantiate(pillarPrefab, position, Quaternion.identity, TerrainParent);
+					break;
+				}
+			}
+		}
+	}
 }

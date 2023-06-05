8b5c46d0884b82620ff478c5390867b09b0ff87b
Abzaparov-Nikolay <nd.abzaparov@gmail.com>
2023-06-05 16:43:26 +0500

    Изменения неудачного мерджа (вроде все работает)

--- DamageReport_Project/Assets/_DamageReport/UI/NewUpgradesPanel/NewUpgradesPanel.cs
+++ DamageReport_Project/Assets/_DamageReport/UI/NewUpgradesPanel/NewUpgradesPanel.cs
@@ -4,22 +4,22 @@ using UnityEngine.Events;
 
 public class NewUpgradesPanel : MonoBehaviour
 {
-    [SerializeField] private NewUpgradesSelector selector;
-    [SerializeField] private ItemPanel itemPanelPrefab;
-    [SerializeField] private WeaponPanel weaponPanelPrefab;
-    [SerializeField] private GameObject panelContainer;
-    [SerializeField] private List<GameObject> relatedPanels;
-    [SerializeField] private Animator risePanelAnimator;
+	[SerializeField] private NewUpgradesSelector selector;
+	[SerializeField] private ItemPanel itemPanelPrefab;
+	[SerializeField] private WeaponPanel weaponPanelPrefab;
+	[SerializeField] private GameObject panelContainer;
+	[SerializeField] private List<GameObject> relatedPanels;
+	[SerializeField] private Animator risePanelAnimator;
 
 	private List<ItemPanel> itemPanels = new();
-    private List<WeaponPanel> weaponPanels = new();
+	private List<WeaponPanel> weaponPanels = new();
 
 
 
-    private void Awake()
-    {
-        selector.OnNewUpgradesAvailable += OnNewUpgrades;
-    }
+	private void Awake()
+	{
+		selector.OnNewUpgradesAvailable += OnNewUpgrades;
+	}
 
 	private void OnDestroy()
 	{
@@ -27,56 +27,59 @@ public class NewUpgradesPanel : MonoBehaviour
 	}
 
 
-private void OnNewUpgrades(List<Item> items, List<Weapon> weapons)
-    {
-        Time.timeScale = 0;
+	private void OnNewUpgrades(List<Item> items, List<Weapon> weapons)
+	{
+		Time.timeScale = 0;
 
 		foreach (var relatedPanel in relatedPanels)
 		{
 			relatedPanel.SetActive(true);
 		}
-		if(risePanelAnimator != null)
+		if (risePanelAnimator != null)
 		{
 			risePanelAnimator.Play("BaseLayer.Rise", 0, 0f);
 		}
 
 
-for (var i = 0; i < items.Count; i++)
-        {
-            var newPanel = Instantiate(itemPanelPrefab, panelContainer.transform);
-            newPanel.SetContent(items[i]);
-            newPanel.OnItemSelected += OnUpgradeSelected;
-            itemPanels.Add(newPanel);
-        }
+		for (var i = 0; i < items.Count; i++)
+		{
+			var newPanel = Instantiate(itemPanelPrefab, panelContainer.transform);
+			newPanel.SetContent(items[i]);
+			newPanel.OnItemSelected += OnUpgradeSelected;
+			itemPanels.Add(newPanel);
+		}
 
-        for (var i = 0; i < weapons.Count; i++)
-        {
-            var newPanel = Instantiate(weaponPanelPrefab, panelContainer.transform);
-            newPanel.SetContent(weapons[i]);
-            newPanel.OnSelected += OnUpgradeSelected;
-            weaponPanels.Add(newPanel);
-        }
-    }
-    {
-    for (var i = itemPanels.Count - 1; i >= 0; i--)
-        {
-            itemPanels[i].OnItemSelected -= OnUpgradeSelected;
-            Destroy(itemPanels[i].gameObject);
-            itemPanels.RemoveAt(i);
-        }
-        
-for (var i = weaponPanels.Count - 1; i >= 0; i--)
-        {
-            weaponPanels[i].OnSelected -= OnUpgradeSelected;
-            Destroy(weaponPanels[i].gameObject);
-            weaponPanels.RemoveAt(i);
-        }
+		for (var i = 0; i < weapons.Count; i++)
+		{
+			var newPanel = Instantiate(weaponPanelPrefab, panelContainer.transform);
+			newPanel.SetContent(weapons[i]);
+			newPanel.OnSelected += OnUpgradeSelected;
+			weaponPanels.Add(newPanel);
+		}
+	}
+	private void OnUpgradeSelected()
 
-        foreach (var relatedPanel in relatedPanels)
-        {
-            relatedPanel.SetActive(false);
-        }
+	{
+		for (var i = itemPanels.Count - 1; i >= 0; i--)
+		{
+			itemPanels[i].OnItemSelected -= OnUpgradeSelected;
+			Destroy(itemPanels[i].gameObject);
+			itemPanels.RemoveAt(i);
+		}
+
+		for (var i = weaponPanels.Count - 1; i >= 0; i--)
+		{
+			weaponPanels[i].OnSelected -= OnUpgradeSelected;
+			Destroy(weaponPanels[i].gameObject);
+			weaponPanels.RemoveAt(i);
+		}
+
+		foreach (var relatedPanel in relatedPanels)
+		{
+			relatedPanel.SetActive(false);
+		}
 
 		Time.timeScale = 1;
 	}
 }
+

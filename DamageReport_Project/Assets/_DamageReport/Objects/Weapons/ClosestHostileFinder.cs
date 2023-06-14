7f38f05fbe20a805379d3cd4948128031b26483b
Lev Nazarov <levndev@gmail.com>
2023-06-14 19:46:10 +0500

    CAR_GAME-101 Пофиксить мини пулемет стреляет себе за спину ( а еще он какой то маленький)

--- DamageReport_Project/Assets/_DamageReport/Objects/Weapons/ClosestHostileFinder.cs
+++ DamageReport_Project/Assets/_DamageReport/Objects/Weapons/ClosestHostileFinder.cs
@@ -6,12 +6,15 @@ public class ClosestHostileFinder : TargetSelector
 {
     [SerializeField] private Team team;
     [SerializeField] private AreaTracker areaTracker;
-
+    [SerializeField] private Transform direction;
     public override bool TryGetTarget(out Transform target)
     {
         target = areaTracker
-			.Where(entity => entity != null && entity.gameObject.activeInHierarchy)
-            .Where(entity => entity.gameObject.TryGetComponentInParent<TeamMember>(out var otherTeamMember) && team.IsHostileTo(otherTeamMember.Team))
+			.Where(entity => entity != null 
+                            && entity.gameObject.activeInHierarchy
+                            && entity.gameObject.TryGetComponentInParent<TeamMember>(out var otherTeamMember)
+                            && team.IsHostileTo(otherTeamMember.Team)
+                            && (direction == null || direction != null && Vector3.Dot(direction.forward, (entity.position - direction.position).normalized) > 0))
 			.MinBy(hostile => Vector3.Distance(transform.position, hostile.position));
         return target != null;
     }
